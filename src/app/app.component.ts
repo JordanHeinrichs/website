6dd18e992440b1cdeefc2502ad9b82020bfbcfc4
Jordan Heinrichs <mail@jordanheinrichs.com>
2019-12-17 16:54:28 -0700

    Pivot and abandon material design
    It was fun to learn more about the material framework but I didn't like
    the look of it so starting fresh with a more minimilistic design.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,10 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { MatIconRegistry } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private breakpointObserver: BreakpointObserver, matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer) {
-    matIconRegistry.addSvgIcon('github',
-      domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg'));
-    matIconRegistry.addSvgIcon('linkedin',
-      domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/linkedin.svg'));
-    matIconRegistry.addSvgIcon('strava',
-      domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/strava.svg'));
-    matIconRegistry.addSvgIcon('facebook',
-      domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/facebook.svg'));
-    matIconRegistry.addSvgIcon('gmail',
-      domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/gmail.svg'));
-  }
-
-  public isHandset: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+  title = 'my-website';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,69 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { HikingComponent } from './hiking/hiking.component';
-import { RunningComponent } from './running/running.component';
-import { SplashPageComponent } from './splash-page/splash-page.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { MyWebsiteComponent } from './projects/project-pages/my-website/my-website.component';
-import { ProjectsOverviewComponent } from './projects/projects-overview/projects-overview.component';
-import { BlogListComponent } from './blog-list/blog-list.component';
-import { RunningOverviewComponent } from './running/running-overview/running-overview.component';
-import { CalgaryMarathonComponent } from './running/running-pages/calgary-marathon/calgary-marathon.component';
-import { BostonMarathonComponent } from './running/running-pages/boston-marathon/boston-marathon.component';
-import { GoldenUltraComponent } from './running/running-pages/golden-ultra/golden-ultra.component';
-import { HikingOverviewComponent } from './hiking/hiking-overview/hiking-overview.component';
-import { RockwallComponent } from './hiking/hiking-pages/rockwall/rockwall.component';
-import { ColumbiaIcefield2018Component } from './hiking/hiking-pages/columbia-icefield2018/columbia-icefield2018.component';
-import { PeytoHutComponent } from './hiking/hiking-pages/peyto-hut/peyto-hut.component';
-import { SolarCarComponent } from './projects/project-pages/solar-car/solar-car.component';
-import { OvalToGpsStravaComponent } from './projects/project-pages/oval-to-gps-strava/oval-to-gps-strava.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SidenavComponent,
-    ProjectsComponent,
-    HikingComponent,
-    RunningComponent,
-    SplashPageComponent,
-    NavbarComponent,
-    FooterComponent,
-    MyWebsiteComponent,
-    ProjectsOverviewComponent,
-    BlogListComponent,
-    RunningOverviewComponent,
-    CalgaryMarathonComponent,
-    BostonMarathonComponent,
-    GoldenUltraComponent,
-    HikingOverviewComponent,
-    RockwallComponent,
-    ColumbiaIcefield2018Component,
-    PeytoHutComponent,
-    SolarCarComponent,
-    OvalToGpsStravaComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
