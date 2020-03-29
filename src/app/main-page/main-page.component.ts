416deefccf7e1cb4e8ce4d3ec67a5af8a3e15e88
Jordan Heinrichs <mail@jordanheinrichs.com>
2020-03-28 21:33:19 -0600

    Get main parallax working with responsive image centering

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import simpleParallax from 'simple-parallax-js';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, AfterViewInit {
+  @ViewChild('main') main: ElementRef;
+  @ViewChild('mainImage') mainImage: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    new simpleParallax(this.mainImage.nativeElement, {
+      delay: 0.4,
+      scale: 1.4,
+      customContainer: this.main.nativeElement
+    } as any);
   }
 
 }
