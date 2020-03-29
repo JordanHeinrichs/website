305767306677f025d2078661bbdabf93ec75f9ba
Jordan Heinrichs <mail@jordanheinrichs.com>
2020-03-29 00:14:49 -0600

    Add in text and image sections

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import simpleParallax from 'simple-parallax-js';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
+  animations: [
+    trigger('loadInFade', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', animate('800ms')),
+      transition(':leave', animate('300ms')),
+    ])
+  ]
+
 })
 export class MainPageComponent implements OnInit, AfterViewInit {
   @ViewChild('main') main: ElementRef;
   @ViewChild('mainImage') mainImage: ElementRef;
+  @ViewChild('secondImage') secondImage: ElementRef;
 
   constructor() { }
 
@@ -21,6 +31,13 @@ export class MainPageComponent implements OnInit, AfterViewInit {
       scale: 1.4,
       customContainer: this.main.nativeElement
     } as any);
+
+    new simpleParallax(this.secondImage.nativeElement, {
+      delay: 0.4,
+      scale: 1.4,
+      customContainer: this.main.nativeElement
+    } as any);
+
   }
 
 }
